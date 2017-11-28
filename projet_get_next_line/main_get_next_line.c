44421cc1ccddccd14c80b269102e8a131f4f16e4
Leo CABANES <lcabanes@student.42.fr>
2017-11-28 21:44:47 +0100

    Ebauche de "get_next_line"

--- projet_get_next_line/main_get_next_line.c
+++ projet_get_next_line/main_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 07:12:57 by lcabanes          #+#    #+#             */
-/*   Updated: 2017/11/28 08:18:43 by lcabanes         ###   ########.fr       */
+/*   Updated: 2017/11/28 21:41:06 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,51 @@ void	ft_putstr(char *str)
 
 int		main(int argc, char **argv)
 {
-	int		fd;
+	int		*fd;
 	int		retour;
 	char	*line;
+	size_t	i;
+	size_t	j;
+	int		temoin;
 
-	if (argc != 2)
+	if (argc < 2)
 	{
 		ft_putstr("NOPE\n");
 		return (0);
 	}
-	fd = open(*(argv + 1), O_RDONLY);
-	while ((retour = get_next_line(fd, &line)) != 0)
+	if (!(fd = (int *)malloc((argc - 1) * sizeof(int))))
+		return (0);
+	i = 0;
+	while (i < (size_t)(argc - 1))
 	{
-		if (retour == -1)
+		*(fd + i) = open(*((argv + 1) + i), O_RDONLY);
+		i++;
+	}
+	i = 0;
+	temoin = 1;
+	while (temoin != 0)
+	{
+		j = 0;
+		temoin = 0;
+		while (j < (size_t)(argc - 1))
 		{
-			ft_putstr("Une erreur est survenue\n");
-			return (0);
+			if ((retour = get_next_line(*(fd + (i % (argc - 1))), &line)) != 0)
+			{
+				temoin = 1;
+			}
+			if (retour == -1)
+			{
+				ft_putstr("Une erreur est survenue\n");
+				return (0);
+			}
+			ft_putstr(line);
+			if (retour != 0)
+			{
+				ft_putstr("\n");
+			}
+			j++;
+			i++;
 		}
-		ft_putstr(line);
-		ft_putstr("\n");
 	}
 	return (0);
 }

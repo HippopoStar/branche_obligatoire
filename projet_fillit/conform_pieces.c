58d734481166c7cebc32184c8e5594ccc96cc9bf
Hephaistos <leocabanes@wanadoo.fr>
2018-01-11 19:41:56 +0100

    Ajout d'un commentaire (a la norme) au fichier "conform_pieces"

--- projet_fillit/conform_pieces.c
+++ projet_fillit/conform_pieces.c
@@ -12,6 +12,36 @@
 
 #include "fillit.h"
 
+/*
+** "match_that_piece" prend en argument :
+** 1. le file_descriptor du fichier "[PROTECT]conform_forms"
+** 2. la piece dont on souhaite vérifier la conformite
+** 3. un buffer de taille 21, present uniquement pour permettre de
+**    recuperer le retour de read
+**
+** dans le corps de la fonction sont declarees les variables :
+** 1. total_read -> permet de stocker la somme des valeurs de retour de read
+** 2. c          -> stock le caractere en cours d'evaluation dans la grille
+** 3. i          -> index de parcours des lignes
+** 4. j          -> index de parcours des colonnes
+**
+** - L'algorithme parcoure caractere par caractere la grille contenant la
+**   piece dont on doit verifier la conformite.
+** - Parallelement, elle lit caractere par caractere la grille du fichier
+**   [PROTECT]conform_forms contenant la piece dont on souhaite checker la
+**   correspondance avec celle dans "one_piece".
+** - Etant donne le pre-traitement de "one_piece", la grille contenant la
+**   piece est constituee exclusivement de points et de lettres de l'alphabet,
+**   et la piece est placee en haut a gauche de la grille.
+** - Ainsi, compte tenu du pre-taitement, il est suffisant de verifier la
+**   correspondance entre les points des 2 grilles pour verifier la correspondance
+**   entre les 2 pieces.
+** - Des lors qu'une divergence est constatee, l'algorithme va lire les caracteres
+**   suivants jusqu'au debut de la grille contenant la piece suivante, afin de
+**   preparer le prochain appel de la fonction, et renvoyer la valeur '0'.
+** - En cas de correspondance, l'algorithme va renvoyer la valeur '1'.
+*/
+
 int		match_that_piece(int fd, char **one_piece, char *read_buf)
 {
 	int		total_read;

5ea14997655830ec651180b458a6ae1036a478ec
Leo CABANES <lcabanes@student.42.fr>
2018-01-16 17:07:35 +0100

    Amelioration du main de test de "get_next_line"

--- projet_get_next_line/main_get_next_line.c
+++ projet_get_next_line/main_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 07:12:57 by lcabanes          #+#    #+#             */
-/*   Updated: 2017/11/28 21:41:06 by lcabanes         ###   ########.fr       */
+/*   Updated: 2018/01/16 16:49:29 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,43 +42,61 @@ int		main(int argc, char **argv)
 	size_t	j;
 	int		temoin;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
-		ft_putstr("NOPE\n");
+		ft_putstr("Le premier argument doit etre '0' ou '1'\
+				(respectivement pour lire les fichiers a la suite les uns des autres, \
+				 et pour lire alternativement une ligne de chaque fichier)\n");
 		return (0);
 	}
-	if (!(fd = (int *)malloc((argc - 1) * sizeof(int))))
+	if (!(fd = (int *)malloc((argc - 2) * sizeof(int))))
 		return (0);
 	i = 0;
-	while (i < (size_t)(argc - 1))
+	while (i < (size_t)(argc - 2))
 	{
-		*(fd + i) = open(*((argv + 1) + i), O_RDONLY);
+		*(fd + i) = open(*((argv + 2) + i), O_RDONLY);
 		i++;
 	}
-	i = 0;
-	temoin = 1;
-	while (temoin != 0)
+	if (*(*(argv + 1) + 0) == '0')
 	{
-		j = 0;
-		temoin = 0;
-		while (j < (size_t)(argc - 1))
+		i = 0;
+		while (i < (size_t)(argc - 2))
 		{
-			if ((retour = get_next_line(*(fd + (i % (argc - 1))), &line)) != 0)
+			while (get_next_line(*(fd + i), &line))
 			{
-				temoin = 1;
+				ft_putstr(line);
+				write(1, "\n", 1);
 			}
-			if (retour == -1)
-			{
-				ft_putstr("Une erreur est survenue\n");
-				return (0);
-			}
-			ft_putstr(line);
-			if (retour != 0)
-			{
-				ft_putstr("\n");
-			}
-			j++;
 			i++;
+		}
+	}
+	else if (*(*(argv + 1) + 0) == '1')
+	{
+		i = 0;
+		temoin = 1;
+		while (temoin != 0)
+		{
+			j = 0;
+			temoin = 0;
+			while (j < (size_t)(argc - 2))
+			{
+				if ((retour = get_next_line(*(fd + (i % (argc - 2))), &line)) != 0)
+				{
+					temoin = 1;
+				}
+				if (retour == -1)
+				{
+					ft_putstr("Une erreur est survenue\n");
+					return (0);
+				}
+				ft_putstr(line);
+				if (retour != 0)
+				{
+					ft_putstr("\n");
+				}
+				j++;
+				i++;
+			}
 		}
 	}
 	return (0);

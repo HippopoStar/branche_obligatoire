620e09e7d550e581260e442d05ba4abb5cc0afe3
Leo CABANES <lcabanes@student.42.fr>
2018-01-31 16:06:35 +0100

    Se renseigner sur la definition d'un Tetrimino fut une brillante idee

--- projet_fillit/tetriminos.c
+++ projet_fillit/tetriminos.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 15:13:01 by lcabanes          #+#    #+#             */
-/*   Updated: 2018/01/31 15:32:36 by lcabanes         ###   ########.fr       */
+/*   Updated: 2018/01/31 16:03:59 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,27 @@ int		tetrimino(char **one_piece)
 {
 	ssize_t	i;
 	ssize_t	j;
-	size_t	count;
+	size_t	count_sharp_mark;
+	size_t	count_bonds;
 
-	count = 0;
-	i = 0;
-	while (i < 4)
-	{
-		j = 0;
-		while (j < 4)
-		{
+	count_sharp_mark = 0;
+	count_bonds = 0;
+	i = -1;
+	while (++i < 4 && (j = -1) != 0)
+		while (++j < 4)
 			if (*(*(one_piece + i) + j) != '.')
 			{
-				count++;
-				if (!((i - 1 >= 0 && *(*(one_piece + (i - 1)) + j) != '.')
-					|| (i + 1 < 4 && *(*(one_piece + (i + 1)) + j) != '.')
-					|| (j - 1 >= 0 && *(*(one_piece + i) + (j - 1)) != '.')
-					|| (j + 1 < 4 && *(*(one_piece + i) + (j + 1)) != '.')))
-				{
-					return (0);
-				}
+				count_sharp_mark++;
+				if (i - 1 >= 0 && *(*(one_piece + (i - 1)) + j) != '.')
+					count_bonds++;
+				if (i + 1 < 4 && *(*(one_piece + (i + 1)) + j) != '.')
+					count_bonds++;
+				if (j - 1 >= 0 && *(*(one_piece + i) + (j - 1)) != '.')
+					count_bonds++;
+				if (j + 1 < 4 && *(*(one_piece + i) + (j + 1)) != '.')
+					count_bonds++;
 			}
-			j++;
-		}
-		i++;
-	}
-	return (count == 4 ? 1 : 0);
+	return ((count_sharp_mark == 4 && !(count_bonds < 6)) ? 1 : 0);
 }
 
 int		tetriminos(char ***pieces)

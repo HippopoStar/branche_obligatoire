0bb4085c70cefe8d6e2e7a88e3a4086aeaaf3677
HippopoStar <leocabanes@wanadoo.fr>
2018-03-01 18:28:19 +0100

    Adaptation de "projet_get_next_line" a l'arborescence exigee pour le rendu

--- projet_get_next_line/libft/srcs_libft/ft_striteri.c
+++ projet_get_next_line/libft/srcs_libft/ft_striteri.c
@@ -1,50 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/09 10:49:38 by lcabanes          #+#    #+#             */
-/*   Updated: 2017/11/28 07:56:47 by lcabanes         ###   ########.fr       */
+/*   Created: 2017/11/10 16:26:20 by lcabanes          #+#    #+#             */
+/*   Updated: 2017/11/28 01:46:01 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "libft.h"
 
-void		ft_putstr(char *str);
-
-static void	ft_putchar(char c)
+static void	aux_ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	write(1, &c, 1);
-}
+	unsigned int	i;
 
-static void	aux_ft_putnbr(int n)
-{
-	if (n != 0)
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		aux_ft_putnbr(n / 10);
-		ft_putchar('0' + (n % 10));
+		f(i, &(*(s + i)));
+		i++;
 	}
 }
 
-void		ft_putnbr(int n)
+void		ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	if (n == 0)
+	if (s != NULL && f != NULL)
 	{
-		ft_putchar('0');
-	}
-	else if (n == -2147483648)
-	{
-		ft_putstr("-2147483648");
-	}
-	else if (n < 0)
-	{
-		ft_putchar('-');
-		aux_ft_putnbr(-n);
-	}
-	else
-	{
-		aux_ft_putnbr(n);
+		aux_ft_striteri(s, f);
 	}
 }

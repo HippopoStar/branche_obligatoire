c3d74e0673e6cf3b04cf75c70bab11e2d0d06046
HippopoStar <leocabanes@wanadoo.fr>
2018-03-29 14:56:41 +0200

    Mise a jour de "ft_atoi.c"

--- projet_get_next_line/libft/srcs_libft/ft_atoi.c
+++ projet_get_next_line/libft/srcs_libft/ft_atoi.c
@@ -12,15 +12,26 @@
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+/*
+** '/t' correspont a 'tabulation horizontale'
+** '/n' correspont a 'retour a la ligne'
+** '/v' correspont a 'tabulation verticale'
+** '/f' correspond a 'saut de page'
+** '/r' correspond a 'retour chariot'
+** ' '  correspond a un espace
+**
+** 'p_o_n' correspond a 'positive_or_negative' dans la fonction 'aux_ft_atoi'
+*/
+
+static long long int	aux_ft_atoi(const char *str)
 {
-	size_t	i;
-	int		n;
+	size_t		i;
+	long long int	n;
 	int		p_o_n;
 
 	i = 0;
-	while (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == '\r'
-			|| *(str + i) == '\v' || *(str + i) == '\f' || *(str + i) == ' ')
+	while (*(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == '\v'
+			|| *(str + i) == '\f' || *(str + i) == '\r' || *(str + i) == ' ')
 	{
 		i++;
 	}
@@ -37,5 +48,13 @@ int		ft_atoi(const char *str)
 		n = (n * 10) + (*(str + i) - '0');
 		i++;
 	}
-	return (p_o_n * n);
+	return ((long long int)p_o_n * n);
+}
+
+int			ft_atoi(const char *str)
+{
+	long long int	n;
+
+	n = aux_ft_atoi(str);
+	return ((-2147483648 <= n && n <= 2147483647) ? (int)n : 0);
 }
